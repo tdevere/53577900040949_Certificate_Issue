0afb132a3effb88ea9ee0679c2edca2e3f541061
tdevere <tdevere@microsoft.com>
2021-09-28 18:51:03 -0500

    first commit

---
+++ 53577900040949_Certificate_Issue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  53577900040949_Certificate_Issue
+//
+//  Created by Tony DeVere on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 53577900040949_Certificate_Issue/_3577900040949_Certificate_IssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3577900040949_Certificate_IssueApp.swift
+//  53577900040949_Certificate_Issue
+//
+//  Created by Tony DeVere on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct _3577900040949_Certificate_IssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
